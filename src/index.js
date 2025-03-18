85a653468519fa4c2212995dcf72e37aced64ebf
sonikaur21 <skaur1902@gmail.com>
2025-03-18 18:26:11 -0400

    added a function and put inside the apikey and apiurl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "79e2b848de54da3deo0aafeff8t7fa08";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
