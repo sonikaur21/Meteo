9b4919ef0d97e79d9cf27ee2aec3eeedfdff3815
sonikaur21 <skaur1902@gmail.com>
2025-03-20 13:19:42 -0400

    added search city and data temperature
    added search city and data temperature, i can now get the city info even if i might capitalize a letter and also got the right temperature from the data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
 function searchCity(city) {
   let apiKey = "79e2b848de54da3deo0aafeff8t7fa08";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
@@ -8,10 +17,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
